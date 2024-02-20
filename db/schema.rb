8ecbc48c16774529bfae902fda070d722f405f24
GillianLu <lu.gillian.nicole@gmail.com>
2024-02-20 17:51:06 +0800

    styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_02_19_130938) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "grubs", force: :cascade do |t|
     t.string "name"
     t.string "cuisine"
     t.integer "calories"
-    t.integer "price"
+    t.string "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
